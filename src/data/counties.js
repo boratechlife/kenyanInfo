4ab6fec3c1466e7066540abff95dd355101f296b
Boratechlife <boratechlife@gmail.com>
2022-09-13 19:09:14 +0300

    add constituencies

--- src/data/counties.js
+++ src/data/counties.js
@@ -3,236 +3,619 @@ export default [
         name:"Mombasa",
         code:"001",
         id:1,
-    },
+        constituencies:[
+        'Changamwe',	
+        'Jomvu',
+        'Kisauni',	
+        'Likoni',	
+        'Mvita',	
+        'Nyali']
+},
     {
         name:"Kwale",
         code:"002",
         id:2,
+        constituencies:[
+            'Kinango',
+            'Lungalunga',	
+            'Matuga',	
+            'Msambweni'
+        ]
     },
     {
         name:"Kilifi",
         code:"003",
         id:3,
+        constituencies:[
+            'Ganze',	
+            'Kaloleni',	
+            'Kilifi North'	,
+            'Kilifi South',	
+            'Magarini',	
+            'Malindi',
+            'Rabai',
+        ]
     },
     {
         name:"Tana River",
         code:"004",
         id:4,
+        constituencies:[
+            'Bura',
+            'Galole',
+            'Garsen'
+    ]
     }, 
     {
         name:"Lamu",
         code:"005",
         id:5,
+        constituencies:[
+        `Chuka/Igambang'ombe`,
+        'Maara'	,
+        'Tharaka'
+    ]
     }, 
     {
         name:"Taita/Taveta",
         code:"006",
         id:6,
+        constituencies:[
+            'Mwatate',	
+            'Taveta	',
+            'Voi	',
+            'Wundanyi',
+        ]
     }, 
     {
         name:"Garissa",
         code:"007",
         id:7,
+        constituencies:[
+        'Balambala',
+        'Dadaab',	
+        'Fafi',	
+        'Garissa Township',	
+        'Ijara',	
+        'Lagdera'
+        ]
     }, 
     {
         name:"Wajir",
         code:"008",
         id:8,
+        constituencies:[
+            'Eldas',	
+            'Tarbaj	',
+            'Wajir East',	
+            'Wajir North	',
+            'Wajir South	',
+            'Wajir West	'
+        ]
     }, 
     {
         name:"Mandera",
         code:"009",
         id:9,
+        constituencies:[
+            'Banissa',
+            'Lafey',	
+            'Mandera East',	
+            'Mandera North',	
+            'Mandera South',
+            'Mandera West'
+        ]
     }, 
     {
         name:"Marsabit",
         code:"010",
         id:10,
+        constituencies:[
+        'Laisamis',	
+        'Moyale',	
+        'North Horr',	
+        'Saku'
+        ]
     }, 
     {
         name:"Isiolo",
         code:"011",
         id:11,
+        constituencies:['Isiolo North',	
+           ' Isiolo South']
     },
     {
         name:"Meru",
         code:"012",
         id:12,
+        constituencies:[
+        'Buuri',	
+        'Central Imenti',	
+        'Igembe Central',	
+        'Igembe North',	
+        'Igembe South',	
+        'North Imenti',	
+        'South Imenti',	
+        'Tigania East',	
+        'Tigania West',
+        ]
     },  
     {
         name:"Tharaka-Nithi",
         code:"013",
         id:13,
-    },
+        constituencies:[
+` Chuka/Igambang'ombe	`,
+' Maara',	
+' Tharaka'
+]
+},
     {
         name:"Embu",
         code:"014",
         id:14,
+        constituencies:[
+            ' Manyatta',	
+            'Mbeere North',	
+            'Mbeere South',	
+            'Runyenjes'
+        ]
     },
 
     {
         name:"Kitui",
         code:"015",
         id:15,
+        constituencies:[
+            'Kitui Central',	
+            'Kitui East',	
+            'Kitui Rural',
+            'Kitui South',
+            'Kitui West',	
+            'Mwingi Central',	
+            'Mwingi North',	
+            'Mwingi West'	
+        ]
     },
     {
         name:"Machakos",
         code:"016",
         id:16,
+        constituencies:[
+        'Kangundo',	
+        'Kathiani',	
+        'Machakos Town',	
+        'Masinga',
+        'Matungulu',	
+        'Mavoko',	
+        'Mwala'	,
+        'Yatta',
+        ]
     },
     {
         name:"Makueni",
         code:"017",
         id:17,
+        constituencies:[
+            ' Kaiti',	
+            'Kibwezi East',	
+            'Kibwezi West',	
+            'Kilome',	
+            'Makueni',	
+            'Mbooni',
+        ]
     }, 
     {
         name:"Nyandarua",
         code:"018",
         id:18,
+        constituencies:[
+            'Kinangop',
+            'Kipipiri',	
+            'Ndaragwa',	
+            'Ol Jorok',	
+            'Ol Kalou',
+        ]
     }, 
     {
         name:"Nyeri",
         code:"019",
         id:19,
+        constituencies:[
+            'Kieni',
+            'Mathira',	
+            'Mukurweini',	
+            'Nyeri Town',	
+            'Othaya',	
+            'Tetu'
+        ]
     },
     {
         name:"Kirinyaga",
         code:"020",
         id:20,
+        constituencies:[
+                'Gichugu',	
+                'Kirinyaga Central',	
+                'Mwea',	
+                'Ndia',
+        ]
     }, 
     {
         name:"Murang'a",
         code:"021",
         id:21,
+        constituencies:[
+                'Gatanga',
+                'Kandara',	
+                'Kangema',	
+                'Kigumo	',
+                'Kiharu',	
+                'Maragwa',
+                'Mathioya'
+        ]
     },  
     {
         name:"Kiambu",
         code:"022",
         id:22,
+        constituencies:[
+        'Gatundu North',	
+        'Gatundu South',	
+        'Githunguri',	
+        'Juja',	
+        'Kabete',	
+        'Kiambaa',	
+        'Kiambu',	
+        'Kikuyu',	
+        'Lari',	
+        'Limuru',	
+        'Ruiru',	
+        'Thika Town'
+        ]
     }, 
     {
         name:"Turkana",
         code:"023",
         id:23,
+        constituencies:[
+            'Loima',	
+            'Turkana Central',
+            'Turkana East',	
+            'Turkana North'	,
+            'Turkana South',	
+            'Turkana West'
+        ]
     },
     {
         name:"West Pokot",
         code:"024",
         id:24,
+        constituencies:[
+            'Kacheliba',
+            'Kapenguria',	
+            'Pokot South',
+            'Sigor',
+        ]
     },
     {
         name:" Samburu",
         code:"025",
         id:25,
+        constituencies:[
+            'Samburu East',	
+            'Samburu North',	
+            'Samburu West'
+        ]
     },
     {
         name:"Trans Nzoia",
         code:"026",
         id:26,
+        constituencies:[
+            'Cherangany	',
+            'Endebess',	
+            'Kiminini',	
+            'Kwanza	',
+            'Saboti'
+        ]
     },
     {
         name:"Uasin Gishu",
         code:"027",
         id:27,
+        constituencies:[
+            'Ainabkoi',
+            'Kapseret',	
+            'Kesses',	
+            'Moiben',	
+            'Soy',
+            'Turbo'	
+        ]
     },
     {
-        name:"Elgeyo/Marakwet",
+        name:"Elgeiyo/Marakwet",
         code:"028",
         id:28,
+        constituencies:[
+        'Keiyo North',
+        'Keiyo South',
+        'Marakwet East'	,
+        'Marakwet West',
+        ]
     },
     {
         name:"Nandi",
         code:"029",
         id:29,
+        constituencies:[
+            ' Aldai',	
+            'Chesumei',	
+            'Emgwen	',
+            'Mosop',	
+            'Nandi Hills',	
+            'Tinderet',
+        ]
     },
     {
         name:"Baringo",
         code:"030",
         id:30,
+        constituencies:[
+            'Baringo North',	
+            'Baringo Central',	
+            'Baringo South',	
+            'Eldama Ravine'	,
+            'Mogotio',
+            'Tiaty',
+        ]
     },
     {
         name:"Laikipia",
         code:"031",
         id:31,
+        constituencies:[
+        'Laikipia East',	
+        'Laikipia North',	
+        'Laikipia West',
+        ]
     },
     {
         name:"Nakuru",
         code:"032",
         id:32,
+        constituencies:[
+            'Bahati',	
+            'Gilgil	',
+            'Kuresoi North',	
+            'Kuresoi South',	
+            'Molo',	
+            'Naivasha',	
+            'Nakuru Town East',	
+            'Nakuru Town West',
+            'Njoro',	
+            'Rongai',	
+            'Subukia',	
+        ]
     },
     {
         name:"Narok",
         code:"033",
         id:33,
+        constituencies:[
+            'Emurua Dikirr',	
+            'Kilgoris',	
+            'Narok East',	
+            'Narok North',
+            'Narok South',
+            'Narok West',
+        ]
     },
     {
         name:"Kajiado",
         code:"034",
         id:34,
+        constituencies:[
+            'Kajiado Central',	
+            'Kajiado East',	
+            'Kajiado North',	
+            'Kajiado South',	
+            'Kajiado West'
+        ]
     },
     {
         name:"Kericho",
         code:"035",
         id:35,
+        constituencies:[
+            ' Ainamoi',	
+            'Belgut',	
+            'Bureti',	
+            'Kipkelion East',	
+            'Kipkelion West	',
+            'Sigowet/Soin'
+        ]
     },
     {
         name:"Bomet",
         code:"036",
         id:36,
+        constituencies:[
+            'Bomet Central',	
+            'Bomet East',	
+            'Chepalungu	',
+            'Konoin',	
+            'Sotik'
+        ]
     },
     {
         name:"Kakamega",
         code:"037",
         id:37,
+        constituencies:[
+            'Butere',	
+            'Ikolomani',	
+            'Khwisero',	
+            'Likuyani',	
+            'Lugari',	
+            'Lurambi	',
+            'Malava',	
+            'Matungu',	
+            'Mumias East',	
+            'Mumias West',
+            'Navakholo',	
+            'Shinyalu'
+        ]
     },
     {
         name:"Vihiga",
         code:"038",
         id:38,
+        constituencies:[
+            'Emuhaya',	
+            'Hamisi	',
+            'Luanda',	
+            'Sabatia',
+            'Vihiga'
+        ]
     },
     {
         name:"Bungoma",
         code:"039",
         id:39,
+        constituencies:[
+            'Bumula',	
+            'Kabuchai',	
+            'Kanduyi',	
+            'Kimilili',	
+            'Mt. Elgon'	,
+            'Sirisia',
+            'Tongaren',	
+            'Webuye East',
+            'Webuye West'
+        ]
     },
     {
         name:"Busia",
         code:"040",
         id:40,
+        constituencies:[
+        'Budalangi',	
+        'Butula	',
+        'Funyula',
+        'Matayos',
+        'Nambale',	
+        'Teso North',	
+        'Teso South'
+        ]
     },
     {
         name:"Siaya",
         code:"041",
         id:41,
+        constituencies:[
+            'Alego Usonga',	
+            'Bondo',	
+            'Gem',
+            'Rarieda',
+            'Ugenya',
+            'Ugunja'
+        ]
     },
     {
         name:"Kisumu",
         code:"042",
         id:42,
+        constituencies:[
+            'Kisumu Central	',
+            'Kisumu East',	
+            'Kisumu West',
+            'Muhoroni',	
+            'Nyakach',
+            'Nyando',
+            'Seme'
+        ]
     },
     {
         name:"Homa Bay",
         code:"043",
         id:43,
+        constituencies:[
+            ' Homa Bay Town',	
+            'Kabondo Kasipul',
+            'Karachuonyo',
+            'Kasipul',
+            'Ndhiwa',	
+            'Rangwe',	
+            'Suba North',
+            'Suba South',
+
+        ]
     },
     {
         name:"Migori",
         code:"044",
         id:44,
+        constituencies:[
+            'Awendo	',
+            'Kuria East',	
+            'Kuria West',
+            'Nyatike	',
+            'Rongo'	,
+            'Suna East',	
+            'Suna West'	,
+            'Uriri'
+        ]
     },
     {
         name:"Kisii",
         code:"045",
         id:45,
+        constituencies:[
+            'Bobasi',	
+            'Bomachoge Borabu',	
+            'Bomachoge Chache',	
+            'Bonchari',	
+            'Kitutu Chache North',
+            'Kitutu Chache South',
+            'Nyaribari Chache',	
+            'Nyaribari Masaba',	
+            'South Mugirango'
+        ]
     },
     {
         name:"Nyamira",
         code:"046",
         id:46,
+        constituencies:[
+            'Borabu',	
+            'Kitutu Masaba',	
+            'North Mugirango',
+            'West Mugirango',
+        ]
     },
     {
         name:"Nairobi City",
         code:"047",
         id:47,
+        constituencies:[
+                'Dagoretti North',	
+                'Dagoretti South',	
+                'Embakasi Central',	
+                'Embakasi East',	
+                'Embakasi North',	
+                'Embakasi South',	
+                'Embakasi West',	
+                'Kamukunji',	
+                'Kasarani',	
+                'Kibra',	
+                'Langata',	
+                'Makadara',	
+                'Mathare',	
+                'Roysambu',	
+                'Ruaraka',	
+                'Starehe',	
+                'Westlands'
+]
     },
 ]
